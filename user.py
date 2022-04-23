0be9ebf774eb1404e2e3d36829a7577dd041776d
Robert Kirui <kirugik79@gmail.com>
2022-04-23 12:23:03 +0300

    Feat: Create display users method to display a list of all registered users

--- user.py
+++ user.py
@@ -6,4 +6,20 @@ class User:
     def __init__(self, full_name, password):
         self.full_name = full_name
         self.password = password
+
+    
+    def save_user(self):
+        User.users.append(self)
+
+    @classmethod
+    def display_users(cls):
+        return cls.users
         
+    @classmethod
+    def login(cls, name, pword):
+        for user in cls.users:
+            if user.full_name == name and user.password == pword:
+                return Credentials.credentials
+            return False 
+
+
