9532254e66c4b5b386a26934c216a24e4536606e
Robert Kirui <kirugik79@gmail.com>
2022-04-23 12:01:03 +0300

    Feat: Create display_credentials method to display user credentials

--- credentials.py
+++ credentials.py
@@ -5,12 +5,13 @@ Credentials class that creates user credentials
 class Credentials:
     # initialize an empty list of credentials
     credentials = []
-    def __init__(self, credential_name, credential_password):
+    def __init__(self, user_password, credential_name, credential_password):
         '''
         the __init__ method defines the properties of the user. It takes the following arguments:
         credential_name: name of the account 
         credential_password: password for the account
         '''
+        self.user_password = user_password
         self.credential_name = credential_name
         self.credential_password = credential_password
 
@@ -22,4 +23,12 @@ class Credentials:
 
     @classmethod
     def display_credentials(cls, password):
-        
+        '''
+        The display_credentials() method returns the list of credentials for the user. It takes user password as a parameter.
+        '''
+        user_credential_list = []
+
+        for credential in cls.credentials:
+            if credential.user_password == password:
+                user_credential_list.append(credential)
+        return user_credential_list
