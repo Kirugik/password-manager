3950de5f6c2dabe805b0e7a50a8d61bef291f705
Robert Kirui <kirugik79@gmail.com>
2022-04-23 11:39:44 +0300

    feat: Create credentials class and initialize it

---
+++ credentials.py
@@ -0,0 +1,6 @@
+class Credentials:
+    # initialize an empty list of credentials
+    credentials = []
+    def __init__(self, credential_name, credential_password):
+        self.credential_name = credential_name
+        self.credential_password = credential_password
